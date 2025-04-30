ecda2933609d742bf4e65f37ef0d9adc6499d75c
Hiran <hiranneels03@gmail.com>
2025-04-30 19:37:46 +0530

    DVC done

--- pipeline.py
+++ pipeline.py
@@ -1,20 +1,25 @@
 # Simplified version using root-level imports
 from fetchdata import fetch
-from model import load_latest_model
+from predict import load_latest_model
 import torch
 from dataeng import scale_input, unscale_predictions
 
+import os
+
 def run_fetch():
+    os.makedirs("data/raw", exist_ok=True)  # ensure directory exists
     data = fetch()
     torch.save(data, "data/raw/latest_data.pt")
 
 def run_predict():
-    model = load_latest_model()
+    os.makedirs("data/processed", exist_ok=True)  # ensure directory exists
+    model = load_latest_model('bidirectional_lstm')
     input_data = torch.load("data/raw/latest_data.pt")
     scaled = scale_input(input_data)
     predictions = model(scaled)
     unscaled = unscale_predictions(predictions)
     torch.save(unscaled, "data/processed/predictions.pt")
+
 
 if __name__ == "__main__":
     import sys
